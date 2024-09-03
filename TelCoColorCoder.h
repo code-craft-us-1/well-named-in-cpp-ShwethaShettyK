d6067252dd7bc81386b4a44d27ade8094fd26107
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-04 00:10:48 +0530

    Update TelCoColorCoder.h

--- TelCoColorCoder.h
+++ TelCoColorCoder.h
@@ -1,22 +1,10 @@
+#pragma once
 #include <iostream>
 #include <assert.h>
+#include <common.h>
 
 namespace TelCoColorCoder
 {
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
-    int numberOfMinorColors =
-        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
     class ColorPair {
         private:
             MajorColor majorColor;
@@ -25,17 +13,10 @@ namespace TelCoColorCoder
             ColorPair(MajorColor major, MinorColor minor):
                 majorColor(major), minorColor(minor)
             {}
-            MajorColor getMajor() {
-                return majorColor;
+            inline MajorColor getMajor() { return majorColor;
             }
-            MinorColor getMinor() {
-                return minorColor;
+            inline MinorColor getMinor() { return minorColor;
             }
-            std::string ToString() {
-                std::string colorPairStr = MajorColorNames[majorColor];
-                colorPairStr += " ";
-                colorPairStr += MinorColorNames[minorColor];
-                return colorPairStr;
-            }
+            std::string ToString(); 
     };
 }

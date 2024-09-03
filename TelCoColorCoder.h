a7f41e66461f3f1a11aac630f81c0ebe8ba31fcf
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-04 00:33:19 +0530

    Update TelCoColorCoder.h

--- TelCoColorCoder.h
+++ TelCoColorCoder.h
@@ -18,5 +18,7 @@ namespace TelCoColorCoder
             inline MinorColor getMinor() { return minorColor;
             }
             std::string ToString(); 
+            ColorPair GetColorFromPairNumber(int pairNumber)const;         
+            int GetPairNumberFromColor(MajorColor major, MinorColor minor)const; 
     };
 }

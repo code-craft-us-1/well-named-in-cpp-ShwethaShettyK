3987bc7f66c96c0f7df84af87d8303a6e4407713
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-06 11:55:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,12 +22,22 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
+void PrintReferenceManual()
+{
+
+    for (int PairNumber = 0; PairNumber <= 25; PairNumber++)
+    {
+        ColorPair colorPair =ColorPair::GetColorFromPairNumber(PairNumber);
+        std::cout << "Pair Number: " << PairNumber << "  Colors: " << colorPair.ToString() << "\n";
+    }
+}
+
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-
+    PrintReferenceManual();
     return 0;
 }

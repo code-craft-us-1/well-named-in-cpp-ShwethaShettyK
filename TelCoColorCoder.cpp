0c6a8a4b5e3e5d1724ea0b2537dedd6843ca91c4
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-10 19:31:34 +0530

    Update TelCoColorCoder.cpp

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -1,48 +1,51 @@
 #pragma once
 #include "TelCoColorCoder.h"
 
-const char* TelCoColorCoder::MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-};
-
-int numberOfMajorColors =
-sizeof(TelCoColorCoder::MajorColorNames) / sizeof(TelCoColorCoder::MajorColorNames[0]);
-
-const char* TelCoColorCoder::MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-int TelCoColorCoder::numberOfMinorColors =
-sizeof(TelCoColorCoder::MinorColorNames) / sizeof(TelCoColorCoder::MinorColorNames[0]);
-
-
-void TelCoColorCoder::PrintReferenceManual()
+namespace TelCoColorCoder
 {
-     int totalPairs = numberOfMajorColors*numberOfMinorColors;
-    for (int PairNumber = 1; PairNumber <= totalPairs; PairNumber++)
+    const char* TelCoColorCoder::MajorColorNames[] = {
+            "White", "Red", "Black", "Yellow", "Violet"
+    };
+
+    int numberOfMajorColors =
+        sizeof(TelCoColorCoder::MajorColorNames) / sizeof(TelCoColorCoder::MajorColorNames[0]);
+
+    const char* TelCoColorCoder::MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+
+    int TelCoColorCoder::numberOfMinorColors =
+        sizeof(TelCoColorCoder::MinorColorNames) / sizeof(TelCoColorCoder::MinorColorNames[0]);
+
+
+    void TelCoColorCoder::PrintReferenceManual()
     {
-        ColorPair colorPair = ColorPair::GetColorFromPairNumber(PairNumber);
-        std::cout << "Pair Number: " << PairNumber << "  Colors: " << colorPair.ToString() << "\n";
+        int totalpairs = numberOfMajorColors * numberOfMinorColors;
+        for (int PairNumber = 1; PairNumber <= totalpairs; PairNumber++)
+        {
+            ColorPair colorPair = ColorPair::GetColorFromPairNumber(PairNumber);
+            std::cout << "Pair Number: " << PairNumber << "  Colors: " << colorPair.ToString() << "\n";
+        }
     }
-}
 
-std::string TelCoColorCoder::ColorPair::ToString()
-{
-    std::string colorPairStr = MajorColorNames[majorColor];
-    colorPairStr += " ";
-    colorPairStr += MinorColorNames[minorColor];
-    return colorPairStr;
-}
+    std::string TelCoColorCoder::ColorPair::ToString()
+    {
+        std::string colorPairStr = MajorColorNames[majorColor];
+        colorPairStr += " ";
+        colorPairStr += MinorColorNames[minorColor];
+        return colorPairStr;
+    }
 
 
-TelCoColorCoder::ColorPair TelCoColorCoder::ColorPair::GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-    MajorColor majorColor =
-        (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    MinorColor minorColor =
-        (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return ColorPair(majorColor, minorColor);
-}
-int TelCoColorCoder::ColorPair::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-    return major * numberOfMinorColors + minor + 1;
+    TelCoColorCoder::ColorPair TelCoColorCoder::ColorPair::GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor =
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int TelCoColorCoder::ColorPair::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
 }
